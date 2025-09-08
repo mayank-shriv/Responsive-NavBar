a9b1dd9495f31e5bbf64a0d6e3013e6b1a6f72e5
Mayank <shrivastavamayank01922@gmail.com>
2025-09-08 12:29:30 +0530

    adding files

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const sidebar = document.getElementById("sidebar");
 const openBtn = document.getElementById("openSidebar");
 const closeBtn = document.getElementById("closeSidebar");
 
-openBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    sidebar.style.display = "flex";
+openBtn.addEventListener("click", () => {
+  sidebar.style.display = "flex";
 });
 
-closeBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    sidebar.style.display = "none";
+closeBtn.addEventListener("click", () => {
+  sidebar.style.display = "none";
 });
